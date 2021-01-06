6a740b5d76af12960f4526d0d25cc88b728dcc9e
Kaye Roberts <pkaye@Kayes-MacBook-Pro.local>
2021-01-05 21:34:15 -0800

    Event works to not using same letter twice, but needs to be troubleshooted

--- script.js
+++ script.js
@@ -18,6 +18,16 @@ let word = []
 let letterBank = ["A", "A", "A", "A", "A", "A", "A", "A", "A", "B", "B", "C", "C", "D", "D", "D", "D", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "F", "F", "G", "G", "G", "H", "H", "I", "I", "I", "I", "I", "I", "I", "I", "I", "J", "K", "L", "L", "L", "L", "M", "M", "N", "N", "N", "N", "N", "N", "O", "O", "O", "O", "O", "O", "O", "O", "P", "P", "Q", "R", "R", "R", "R", "R", "R", "S", "S", "S", "S", "T", "T", "T", "T", "T", "T", "U", "U", "U", "U", "V", "V", "W", "W", "X", "Y", "Y", "Z"]
 // for (let i = 0; i < letterBank.length; i++)
 
+//Stack Overflow
+A = E = I = L = N = O = R = S = T = U = 1
+D = G = 2
+B = C = M = P = 3
+F = H = V = W = Y = 4
+K = 5
+J = X = 8
+Q = Z = 10
+
+
 let letterBoard = [];
 
 //NOTE //Getting 16 random letters from the letter bank
@@ -54,6 +64,9 @@ const gameSquares = {
     $sq16: $('#16'), 
 }
 
+const squareValue = Object.keys(gameSquares)
+for(let x = 0; x < squareValue.length; x++)
+
 // gameSquares.$sq1.html(letterBoard)
 
 //NOTE //Looping through the object to place letters on the board
@@ -80,6 +93,7 @@ gameSquares.$sq14.html(letterBoard[13])
 gameSquares.$sq15.html(letterBoard[14])
 gameSquares.$sq16.html(letterBoard[15])
 
+
 //NOTE Timer - Stack Overflow
 let counter = 60 
 let countdown = setInterval(function(){
@@ -105,18 +119,23 @@ const table = document.querySelector("table")
 let words = table.addEventListener('click', evt => {
     let chosenLetter = evt.target.innerText
     console.log(chosenLetter)
-    evt.target.style.backgroundColor = "#c2e3e3"
-    $("#chosen-letters").append(`<div class='game-ltrs'>${chosenLetter}</div>`)
-    
-    word.push(chosenLetter)
+
+    if(chosenLetter === word[word.length - 1]){
+        alert('Pick a different letter')
+    } else {
+        evt.target.style.backgroundColor = "#c2e3e3"
+        $("#chosen-letters").append(`<div class='game-ltrs'>${chosenLetter}</div>`)
+        word.push(chosenLetter)
+    }
 })
 
 console.log(word)
 
+//NOTE // Clears the chosen letters field
+const tableSq = document.getElementById('table')
 const clearBtn = document.getElementById("clear")
 let clearWord = clearBtn.addEventListener('click', evt => {
 
-    let tblStyle = $('table')
     let submitWords = $('.game-ltrs')
     if(submitWords.length > 0){
         $('.game-ltrs').remove()
@@ -124,7 +143,7 @@ let clearWord = clearBtn.addEventListener('click', evt => {
         word = []
         
         //Have to come back to this - getting the table to reset it's background color to white
-        // document.getElementById('table').style.backgroundColor = "white"
+        // tblStyle.style.backgroundColor = "white"
     }
 })
 
@@ -134,11 +153,15 @@ let submitWord = submitBtn.addEventListener('click', evt => {
 
     if(word.length >= 3 && word.length <= 15){
     let listItm = document.createElement("li")
+
     let fullWrd = document.createTextNode(word.join(''))
     listItm.appendChild(fullWrd)
     document.getElementById("word-list").appendChild(listItm)
 
     word = []
+
+    $('.game-ltrs').remove()
+
     } else if(word.length <= 2){
         alert('Word too short - need at least 3 characters')
     } 
